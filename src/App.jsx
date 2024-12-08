c593e5903e04e67f172d6de1a6ee3751b47c4077
Suyash Chavan <suyashchavanofficial@gmail.com>
2024-12-09 03:09:16 +0530

    Created Projects section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { About } from './components/About/About'
 import { Experience } from './components/Experience/Experience'
 import { Hero } from './components/Hero/Hero'
 import { Navbar } from './components/Navbar/Navbar'
+import { Projects } from './components/Projects/Projects'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
         <Hero />
         <About />
         <Experience />
+        <Projects />
       </div>
     </>
   )
