1c4beea73c91b82e66b1712f6f65ad1c0d1eaf34
Suyash Chavan <suyashchavanofficial@gmail.com>
2025-07-06 17:18:10 +0530

    Updated Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-
-import styles from './App.module.css'
-import { About } from './components/About/About'
-import { Contact } from './components/Contact/Contact'
-import { Experience } from './components/Experience/Experience'
-import { Hero } from './components/Hero/Hero'
-import { Navbar } from './components/Navbar/Navbar'
-import { Projects } from './components/Projects/Projects'
+import styles from "./App.module.css";
+import { About } from "./components/About/About";
+import { Contact } from "./components/Contact/Contact";
+import { Experience } from "./components/Experience/Experience";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
 function App() {
-
   return (
     <>
       <div className={styles.App}>
@@ -18,9 +16,10 @@ function App() {
         <Experience />
         <Projects />
         <Contact />
+        
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
